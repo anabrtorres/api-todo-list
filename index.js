729430cdadff0df9fe0f85262bcf2aa22413d3a7
Ana <ana.torres@marcenariadiferente.com>
2022-12-12 13:41:34 -0300

    commit inicial

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
 
-const estudante_controller = require('./estudantes-controller')
-
-mongoose.connect('mongodb+srv://usuario:senha@cluster0.nkc2c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.Promise = global.Promise
-try {
-    let db = mongoose.connection
-    db.on('errr', console.error.bind(console, 'erro de conexao no banco'))
-} catch (e) {
-    console.log(e)
-}
-
-const router = express.Router()
-
 const app = express()
 
 app.use(express.urlencoded({ extended: true }));
@@ -22,13 +9,6 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('teste oi')
 })
-
-router.post('/estudantes', estudante_controller.cadastrarEstudante)
-router.get('/estudantes', estudante_controller.listarEstudantes)
-router.get('/estudantes/:id', estudante_controller.buscarEstudante)
-
-app.use('/', router)
-
 
 let porta = process.env.PORT || 4000
 
